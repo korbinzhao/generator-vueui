4f92f6e324ecddc1024d2eaf61722eea30763fda
zhaobin.zb <zhaobin.zb@alibaba-inc.com>
2018-01-12 19:09:17 +0800

    在postcssrc.js中增加px2rem

--- app/templates/.postcssrc.js
+++ app/templates/.postcssrc.js
@@ -5,6 +5,13 @@ module.exports = {
   "plugins": {
     // to edit target browsers: use "browserslist" field in package.json
     "postcss-import": {},
-    "autoprefixer": {}
+    "autoprefixer": {},
+    "postcss": [require('postcss-px2rem')({
+      baseDpr: 2,             // base device pixel ratio (default: 2)
+      threeVersion: false,    // whether to generate @1x, @2x and @3x version (default: false)
+      remVersion: true,       // whether to generate rem version (default: true)
+      remUnit: 72,            // rem unit value (default: 75)
+      remPrecision: 6         // rem precision (default: 6)
+    })]
   }
 }
--- app/templates/src/demo-component.vue
+++ app/templates/src/demo-component.vue
@@ -1,10 +1,13 @@
 <style scoped lang="scss">
+.text {
+  font-size: 28px;
+}
 </style>
 <template>
-  <div>demo-component</div>
+  <div class="text">demo-component</div>
 </template>
 <script>
 export default {
   name: "demo-component"
-}
+};
 </script>
